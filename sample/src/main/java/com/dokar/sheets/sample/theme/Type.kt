081c9d410eede037c7b82cf567c8f97b6a81b343
dokar3 <68095777+dokar3@users.noreply.github.com>
2023-01-12 11:49:41 +0800

    Change the font weight of sample texts to normal

--- sample/src/main/java/com/dokar/sheets/sample/theme/Type.kt
+++ sample/src/main/java/com/dokar/sheets/sample/theme/Type.kt
@@ -10,7 +10,7 @@ import androidx.compose.ui.unit.sp
 val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.SemiBold,
+        fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
     /* Other default text styles to override
