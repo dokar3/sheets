335e7a0d3a6d167d11ceb883f2cb95a4a6653e94
Ivan Pavlov <solovey-vanyaaa@yandex.ru>
2024-07-09 15:17:59 +0300

    Fixes for build worker

--- sheets/src/commonMain/kotlin/com/dokar/sheets/BottomSheet.material.kt
+++ sheets/src/commonMain/kotlin/com/dokar/sheets/BottomSheet.material.kt
@@ -8,6 +8,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
+import com.dokar.sheets.dialog.CoreBottomSheet
+import com.dokar.sheets.layout.CoreBottomSheetLayout
 
 /**
  * A bottom sheet will be displayed in a dialog window.
