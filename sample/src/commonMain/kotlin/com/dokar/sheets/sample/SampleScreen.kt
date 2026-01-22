850e4cc275bdfc2a5380eb067f467488c493819e
dokar3 <68095777+dokar3@users.noreply.github.com>
2026-01-22 20:56:04 +0800

    Remove unused import

--- sample/src/commonMain/kotlin/com/dokar/sheets/sample/SampleScreen.kt
+++ sample/src/commonMain/kotlin/com/dokar/sheets/sample/SampleScreen.kt
@@ -45,7 +45,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawWithCache
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.layout.onSizeChanged
